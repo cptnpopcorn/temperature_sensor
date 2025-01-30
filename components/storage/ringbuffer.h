38124634b7d2cd846d53a7d5f67375db6dbf3baf
Hagen Hentschel <hagen@hentschel.ch>
2025-01-30 21:38:09 +0100

    fix ringbuffer iterator for saturated buffer case

--- components/storage/ringbuffer.h
+++ components/storage/ringbuffer.h
@@ -7,15 +7,14 @@
 template <class Item, std::size_t Size>
 class ringbuffer_iterator final {
  private:
-  using iterator_t = std::array<Item, Size>::const_iterator;
+  using array_t = std::array<Item, Size>;
 
  public:
   ringbuffer_iterator(const std::array<Item, Size>& items, size_t pos) noexcept
-      : pos{items.cbegin() + pos}, start{items.cbegin()}, end{items.cend()} {}
+      : items{items}, pos{pos} {}
 
   ringbuffer_iterator& operator++() noexcept {
-    auto next_pos = ++pos;
-    pos = next_pos == end ? start : next_pos;
+    ++pos;
     return *this;
   }
 
@@ -23,12 +22,11 @@ class ringbuffer_iterator final {
     return pos != other.pos;
   }
 
-  const Item& operator*() { return *pos; }
+  const Item& operator*() { return items[pos % items.size()]; }
 
  private:
-  iterator_t pos;
-  const iterator_t start;
-  const iterator_t end;
+  const array_t& items;
+  size_t pos;
 };
 
 template <class Item, std::size_t Size>
@@ -40,24 +38,27 @@ class ringbuffer final {
   size_t size() const noexcept { return write_size; }
 
   void write(const Item& item) noexcept {
-    storage[write_position] = item;
-    write_position = (write_position + 1) % storage.size();
-    write_size = std::min(write_size + 1, storage.size());
+    items[write_position] = item;
+    write_position = (write_position + 1) % items.size();
+    write_size = std::min(write_size + 1, items.size());
   }
 
   const_iterator begin() const noexcept {
-    return {storage,
-            (storage.size() + write_position - write_size) % storage.size()};
+    return {items, (items.size() + write_position - write_size) % items.size()};
   }
 
-  const_iterator end() const noexcept { return {storage, write_position}; }
+  const_iterator end() const noexcept {
+    return {items, write_position >= write_size
+                       ? write_position
+                       : write_position + items.size()};
+  }
 
   void flush(size_t size) noexcept { write_size -= size; }
 
  private:
   size_t write_size;
   size_t write_position;
-  std::array<Item, Size> storage;
+  std::array<Item, Size> items;
 };
 
 #endif /* E56756F2_FCE9_4506_8291_7FDAF2C00E14 */
